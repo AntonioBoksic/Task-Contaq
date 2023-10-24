0577531966105ab26c851b8b20e4ff7669c2d2b9
Antonio <antonioboksic1603@gmail.com>
2023-10-24 19:43:21 +0200

    Iniziato ad impostare il componente Navbar.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,3 +1,22 @@
+<template>
+  <nav>
+    <!-- Se non loggato, mostra Registrati e Log in -->
+    <div v-if="!isLoggedIn">
+      <a @click="goToRegister">Registrati</a>
+      <a @click="goToLogin">Log in</a>
+    </div>
+
+    <!-- Se loggato, mostra il nome utente e il menu a tendina -->
+    <div v-else>
+      <span>{{ username }}</span>
+      <div>
+        <!-- Qui potresti avere ulteriori opzioni nel menù a tendina, ad es. logout -->
+        <button @click="logout">Logout</button>
+      </div>
+    </div>
+  </nav>
+</template>
+
 <script>
 export default {
   name: 'Navbar',
@@ -27,33 +46,18 @@ export default {
 };
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
-</template>
-
 <style scoped>
-.read-the-docs {
-  color: #888;
+nav {
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  width: 100%;
+  border: 1px solid white;
+  height: 5vh;
+}
+
+nav a {
+  padding: 5px;
+  cursor: pointer;
 }
 </style>
